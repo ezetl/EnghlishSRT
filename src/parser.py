de5042ec407dbf0f50cdfb1643450a75230ebeca
Ezequiel <ezetl91@gmail.com>
2015-03-04 18:46:20 -0300

    parser outline

---
+++ src/parser.py
@@ -0,0 +1,19 @@
+class Parser(object):
+    """
+    Simple parser for subtitles in .srt format.
+    'times' and 'text' are arrays that contain the times
+    of appearance of each text and the text itself, respectively.
+    """
+    def __init__(self, subtitle=""):
+        self.subtitle = subtitle
+        self.times = []
+        self.text = []
+
+    def parse(self):
+        return
+
+    def get_times():
+        return self.times
+
+    def get_text(self):
+        return self.text
