b9e9390e15e2b69842421bc04d2dc8b1edc30a2f
Ezequiel <ezetl91@gmail.com>
2015-03-05 15:10:49 -0300

    normalizer skeleton

---
+++ src/normalizer.py
@@ -0,0 +1,29 @@
+import nltk
+
+
+class Normalizer(object):
+    def __init__(self):
+        self.tokens = []
+        self.pos_tags = []
+        self.lemmas = []
+
+    def tokenize(self, text):
+        return
+
+    def do_pos_tag(self):
+        return
+
+    def filter_tags(self):
+        return
+
+    def lemmatize(self):
+        return
+
+    def normalize(self, text):
+        self.tokens = self.tokenize(text)
+        self.pos_tags = self.do_pos_tag()
+        self.filter_tags()
+        self.lemmatize()
+
+    def get_final_lemmas(self):
+        return self.lemmas
