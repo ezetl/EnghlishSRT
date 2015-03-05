b7d541c856aa36671a9af3c1e5c31b3b9621b7e5
Ezequiel <ezetl91@gmail.com>
2015-03-05 15:22:00 -0300

    subtitle skeleton added

---
+++ src/subtitle.py
@@ -0,0 +1,19 @@
+class Subtitle(object):
+    """
+    This is the new subtitle that has to be saved.
+    All the formatting and the inclusion of definitions in the final subtitle
+    go here.
+    """
+    def __init__(self, times=[], text=[], lemmas=[], filename="sub.srt"):
+        self.times = times
+        self.text = text
+        self.final_text = ""
+        self.lemmas = lemmas
+        self.filename = filename
+
+    def create_subtitle(self):
+        return
+
+    def save(self):
+        with open(self.filename, 'w') as f:
+            f.write(self.final_text)
